0771fa94552a6fde86987f62c8fdc6a9a254bfcf
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-04-14 21:14:10 -0400

    add update and delete endpoints

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -20,16 +20,53 @@ router.post('/', async (req, res) => {
 	console.log('carData', carData);
 
 	try {
-		console.log('Trying');
-
 		const car = await db('cars').insert(carData);
 
 		console.log('after await');
 
 		res.status(201).json(car);
 	} catch (err) {
-		res.status(500).json({ message: 'Internal Server Error', error: err });
+		res.status(500).json({ message: 'Internal Server Error', error: err.message });
 	}
+});
+
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+	db('cars')
+		.where({ id })
+		.first()
+		.then((car) => {
+			res.status(200).json(car);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Internal Server Error' });
+		});
+});
+
+router.put('/:id', async (req, res) => {
+	const carData = req.body;
+	const { id } = req.params;
+
+	try {
+		const car = await db('cars').where({ id }).update(carData);
+		res.status(201).json(car);
+	} catch (error) {
+		res.status(500).json({ message: 'Internal Server Error', error: err.message });
+	}
+});
+
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+
+	db('cars')
+		.where({ id })
+		.del()
+		.then((car) => {
+			res.status(200).json(car);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Internal Server Error', error: err.message });
+		});
 });
 
 module.exports = router;
